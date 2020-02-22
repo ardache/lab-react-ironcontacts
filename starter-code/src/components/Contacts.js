cca7cc93a27a51ddc44ef7a174bbf8a5ca23280f
Ardash <ardache@hotmail.com>
2020-02-22 15:59:09 -0600

    initializing hooks

--- starter-code/src/components/Contacts.js
+++ starter-code/src/components/Contacts.js
@@ -3,13 +3,11 @@ import React from 'react';
 const Contact = props => {
     return (
         <div className = "table">
-           
-                   
-                        <tr>
-                            <td>       <img alt="" src={props.picture} />                </td>
-                            <td>       {props.name}                  </td>
-                            <td>       {props.popularity}                  </td>
-                        </tr>
+            <tr>
+                <td>       <img alt="" src={props.picture} />                </td>
+                <td>       {props.name}                  </td>
+                <td>       {props.popularity}                  </td>
+            </tr>
         </div>
     )
 }
